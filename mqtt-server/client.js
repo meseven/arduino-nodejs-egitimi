4219f0a1ee5b0eb2930acea94618e17301727875
Mehmet Seven <mehmetseven0@gmail.com>
2019-09-21 19:22:32 +0300

    mqtt client pub/sub

--- mqtt-server/client.js
+++ mqtt-server/client.js
@@ -1,6 +1,14 @@
 var mqtt = require('mqtt');
 var client = mqtt.connect('mqtt://127.0.0.1:1883');
 
+client.subscribe('new-user');
+
 client.on('connect', function() {
 	console.log('connected!');
+
+	client.publish('new-user', 'Mehmet-' + Math.ceil(Math.random() * 10));
+});
+
+client.on('message', function(topic, message) {
+	console.log(topic, ' : ', message.toString());
 });
--- mqtt-server/server.js
+++ mqtt-server/server.js
@@ -16,7 +16,7 @@ server.on('clientDisconnected', function(client) {
 
 // fired when a message is received
 server.on('published', function(packet, client) {
-	console.log('Published', packet.payload);
+	console.log('Published', packet.payload.toString());
 });
 
 server.on('ready', setup);
