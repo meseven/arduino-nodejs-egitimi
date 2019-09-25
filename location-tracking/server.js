c98aed72a4516073b9ae895f0e4d0ba2e677d1ef
Mehmet Seven <mehmetseven0@gmail.com>
2019-09-25 22:33:35 +0300

    location tracking project

---
+++ location-tracking/server.js
@@ -0,0 +1,35 @@
+var mosca = require('mosca')
+
+var ascoltatore = {
+  type: 'redis',
+  redis: require('redis'),
+  db: 12,
+  port: 6379,
+  return_buffers: true, // to handle binary payloads
+  host: "localhost"
+};
+
+var moscaSettings = {
+  port: 1883,
+  backend: ascoltatore,
+  persistence: {
+    factory: mosca.persistence.Redis
+  }
+};
+
+var server = new mosca.Server(moscaSettings);
+server.on('ready', setup);
+
+server.on('clientConnected', function(client) {
+	console.log('client connected', client.id);		
+});
+
+// fired when a message is received
+server.on('published', function(packet, client) {
+  console.log('Published', packet.topic, packet.payload);
+});
+
+// fired when the mqtt server is ready
+function setup() {
+  console.log('Mosca server is up and running')
+}
